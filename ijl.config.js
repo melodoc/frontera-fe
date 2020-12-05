8d4423d8c97b47bcaabdbc01687db3b92278c197
melodoc <liubov_rz@yahoo.com>
2020-12-05 18:52:33 +0300

    Update build

--- ijl.config.js
+++ ijl.config.js
@@ -2,9 +2,40 @@ const pkg = require('./package')
 
 module.exports = {
     "apiPath": "stubs/api",
-    "webpackConfig": {
-        "output": {
-            "publicPath": `/static/frontera/${pkg.version}/`
-        }
+    webpackConfig: {
+        output: {
+            publicPath: `/static/frontera/${pkg.version}/`
+        },
+        module: {
+          rules: [
+            {
+              test: /\.css$/i,
+              use: [
+                'style-loader',
+                {
+                  loader: 'css-loader',
+                  options: {
+                    modules:true,
+                  }
+                },
+                {
+                  loader: 'postcss-loader',
+                  options: {
+                    postcssOptions: {
+                      plugins: [
+                        [
+                          "postcss-preset-env",
+                          {
+                            // Options
+                          },
+                        ],
+                      ],
+                    },
+                  },
+                },
+              ],
+            },
+          ],
+        },
     },
 }
