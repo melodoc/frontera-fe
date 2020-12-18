c3edfe52ffd513fee01ab58eb6bb8cb458f5c201
Timofeeva.Yul <Timofeeva.Y2@digdes.com>
2020-12-18 16:25:03 +0300

    FJ-3 - Создание заглушек

--- ijl.config.js
+++ ijl.config.js
@@ -1,10 +1,13 @@
-const pkg = require('./package')
+const pkg = require('./package');
 
 module.exports = {
     "apiPath": "stubs/api",
     "webpackConfig": {
         "output": {
             "publicPath": `/static/frontera/${pkg.version}/`
-        }
+        },
     },
+    config: {
+        'frontera.login': '/api/login'
+    }
 }
