73904a19a94cac880a72d3c3af945d53561a708e
melodoc <liubov_rz@yahoo.com>
2020-12-18 14:07:35 +0300

    fe-9 Add init page components

---
+++ src/components/page-header/page-header.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import style from "./style.css";
+
+interface PageHeaderProps {
+  readonly color: string;
+  readonly label: string;
+  readonly size: string;
+}
+
+type PageHeaderType = React.FunctionComponent<PageHeaderProps>;
+
+const PageHeader: PageHeaderType = ({color, label, size}: PageHeaderProps) => {
+  if (color === "purple") {
+      return <h1 className={style.pageHeader}>{label}</h1>;
+  }
+    return <h1 className={style.pageHeaderWhite}>{label}</h1>;
+};
+
+export default PageHeader;
