4740e01b55c52964da8254cd86ecac233aebbc3d
melodoc <liubov_rz@yahoo.com>
2020-12-23 03:08:13 +0300

    FJ-17 Обновлена верстка контейнеров auth

--- src/containers/auth/registration/index.tsx
+++ src/containers/auth/registration/index.tsx
@@ -1,22 +1,20 @@
 import React from 'react';
 
-import IconLogo from '../../../components/icon-logo/icon-logo';
+import HeaderForm from '../../../components/header-form';
 import TitleWithSubtitle from '../../../components/title-with-subtitle/title-with-subtitle';
 import Input from '../../../components/input/input';
 import Button from '../../../components/button/button';
 
 import style from '../style.css';
 
+const COLOR = 'purple';
+
 const Registration = () => (
-  <div className={style.body}>
-    <header className={style.header}>
-    <nav className={style.nav}>
-          <IconLogo color={'white'} />
-      </nav>
-    </header>
+  <React.Fragment>
+    <HeaderForm color={COLOR} />
     <main className={style.container}>
       <section className={style.login}>
-        <TitleWithSubtitle color={'white'} type={'registartion'} />
+        <TitleWithSubtitle color={COLOR} type={'registartion'} />
       </section>
       <form method={'post'} action={'https://echo.imaginary-server.ru'}>
         <div className={style.form__wrapper}>
@@ -33,15 +31,15 @@ const Registration = () => (
             <Input type={'repeatPassword'} />
           </p>
         </div>
-        <div className={style.form__buttons }>
-          <Button color={'white'} name={'signUp'} />
+        <div className={style.form__buttons}>
+          <Button color={COLOR} name={'signUp'} />
         </div>
       </form>
       <div className={style.login__buttons}>
-          <Button color={'white'} name={'cancel'} />
+        <Button color={COLOR} name={'cancel'} />
       </div>
     </main>
-  </div>
+  </React.Fragment>
 );
 
 export default Registration;
