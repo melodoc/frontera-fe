03ba52bc7581fc2fa17b0f8c309d82549fe8e1cb
Ivan Belyanin <belyanin-2001@list.ru>
2020-12-25 11:41:33 +0400

    FJ-9 Убрал лишний код

--- src/containers/homepage/index.tsx
+++ src/containers/homepage/index.tsx
@@ -4,15 +4,6 @@ import Preview from './../../components/preview';
 import Trending from './../../components/trending';
 
 const HomePage = () => {
-    const [state, setState] = useState(false)
-    const toggler = () => {
-        setState(!state)
-    }
-
-    useEffect(() => {
-        fetch("yandex.ru")
-    }, [state])
-
     return (
     <>
         <Header/>
@@ -20,35 +11,5 @@ const HomePage = () => {
         <Trending/>
     </>
 )};
-
-class HomePageclass extends React.Component {
-    constructor(props){
-        super (props)
-        this.state = false
-        this.toggler = this.toggler.bind(this)
-    }
-    toggler() {
-        this.setState(!this.state)
-    }
-    componentDidMount(){
-        fetch("yandex.ru")
-    }
-    componentDidUpdate(){
-        fetch("yandex.ru")
-    }
-    render(){
-        return(
-            <>
-                <Header/>
-                <Preview/>
-                <Trending/>
-            </>
-        )
-    }
-}
-
-class NewPageClassc extends HomePageclass {
-    
-}
 
 export default HomePage
