9fee28e5ba800a01f717dac4663c1f7cff0087be
melodoc <liubov_rz@yahoo.com>
2021-01-23 15:20:10 +0300

    FJ-26 Исправлены стили форм

--- src/containers/auth/restore/index.tsx
+++ src/containers/auth/restore/index.tsx
@@ -22,28 +22,28 @@ class Restore extends React.Component {
       <React.Fragment>
         <HeaderForm />
         <main className={style.container}>
-          <section className={style.login}>
+          <section className={style.form}>
             <TitleWithSubtitle type={'restore'} />
-          </section>
-          <form method="post" action="#">
-            <div className={style.formWrapper}>
-              <Input inputRef={this.firstInputRef} label="Email Address" id="restoreEmail" name="email" type="email" placeholder="email@example.com" />
-            </div>
+            <form method="post" action="#">
+              <div className={style.formWrapper}>
+                <Input inputRef={this.firstInputRef} label="Email Address" id="restoreEmail" name="email" type="email" placeholder="email@example.com" />
+              </div>
+              <div className={style.buttons}>
+                <LinkButton
+                  to={URLs.resetpassword.url}
+                  as={ConnectedLink}>
+                  Reset
+              </LinkButton>
+              </div>
+            </form>
             <div className={style.buttons}>
               <LinkButton
-                to={URLs.resetpassword.url}
+                to={URLs.login.url}
                 as={ConnectedLink}>
-                Reset
-              </LinkButton>
-            </div>
-          </form>
-          <div className={style.buttons}>
-            <LinkButton
-              to={URLs.login.url}
-              as={ConnectedLink}>
-              Cancel
+                Cancel
             </LinkButton>
-          </div>
+            </div>
+          </section>
         </main>
       </React.Fragment>
     );
