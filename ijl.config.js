4af01ec9f1f927b6ad92e2841efddcaf1e630881
Diana <dinusya91@gmail.com>
2021-02-22 18:45:38 +0500

    Детальная информация по курсу | верстка

--- ijl.config.js
+++ ijl.config.js
@@ -10,55 +10,53 @@ module.exports = {
             publicPath: `/static/${pkg.name}/${process.env.VERSION || pkg.version}/`
         },
         module: {
-          rules: [
-            {
-              test: /\.css$/i,
-              use: [
-                { loader: 'style-loader', },
-                {
-                    loader: 'css-loader',
-                    options: {
-                        modules: {
-                            mode: 'local',
-                            exportGlobals: true,
-                            localIdentName: isProd ?
-                                '[hash:base64]'
-                                : '[path]--[name]__[local]--[hash:base64:3]',
-                            localIdentContext: path.resolve(__dirname, 'src'),
-                            exportLocalsConvention: "camelCase",
-                        },
-                    }
-                },
-                {
-                  loader: 'postcss-loader',
-                  options: {
-                    postcssOptions: {
-                      plugins: [
-                        [
-                          'postcss-preset-env',
-                          {
-                            // Options
-                          },
-                        ],
-                        
-                      ],
+            rules: [{
+                test: /\.css$/i,
+                use: [
+                    { loader: 'style-loader', },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: {
+                                mode: 'local',
+                                exportGlobals: true,
+                                localIdentName: isProd ?
+                                    '[hash:base64]' :
+                                    '[path]--[name]__[local]--[hash:base64:3]',
+                                localIdentContext: path.resolve(__dirname, 'src'),
+                                exportLocalsConvention: "camelCase",
+                            },
+                        }
                     },
-                  },
-                },
-              ],
-            },
-          ],
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: [
+                                    [
+                                        'postcss-preset-env',
+                                        {
+                                            // Options
+                                        },
+                                    ],
+
+                                ],
+                            },
+                        },
+                    },
+                ],
+            }, ],
         },
     },
     navigations: {
-      'frontera':'/frontera',
-      'link.frontera.login':'/login',
-      'link.frontera.personalizations':'/personalizations',
-      'link.frontera.suggestions':'/suggestions',
-      'link.frontera.home':'/home',
-      'link.frontera.registration':'/registration',
-      'link.frontera.resetpassword':'/resetpassword',
-      'link.frontera.coursepage':'/coursepage',
+        'frontera': '/frontera',
+        'link.frontera.login': '/login',
+        'link.frontera.personalizations': '/personalizations',
+        'link.frontera.suggestions': '/suggestions',
+        'link.frontera.home': '/home',
+        'link.frontera.registration': '/registration',
+        'link.frontera.resetpassword': '/resetpassword',
+        'link.frontera.coursepage': '/coursepage',
     },
     config: {
         'frontera.api': '/api'
