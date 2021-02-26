f1236b59f5c4e7ee80cf7df96f2b4b8e08e78ba4
melodoc <liubov_rz@yahoo.com>
2021-02-26 17:37:01 +0300

    Added error state

--- src/pages/auth/steps/login/index.tsx
+++ src/pages/auth/steps/login/index.tsx
@@ -6,12 +6,12 @@ import { useTranslation } from 'react-i18next';
 
 import { URLs } from '../../../../__data__/urls';
 import { getLogin } from '../../../../__data__/action/login';
+import { reset } from '../../../../__data__/slice/login';
 
 import Button from '../../../../components/button'
 import Input from '../../../../components/input';
 
 import style from './style.css';
-import { stat } from 'fs';
 
 const Login = ({ moveNextStep }) => {
     const { t } = useTranslation();
@@ -20,30 +20,37 @@ const Login = ({ moveNextStep }) => {
     const [login, setLogin] = useState('');
     const [password, setPassword] = useState('');
     const token = useSelector((state: any) => state.login.token);
-    const errors = useSelector((state: any) => state.login.errors);
-
-
+    const errors = !!useSelector((state: any) => state.login.errors);
+    
     const handleChangeLogin = (event) => {
         setLogin(event.target.value);
+
+        if (errors) {
+            dispatch(reset());
+        }
     };
 
     const handleChangePassword = (event) => {
         setPassword(event.target.value);
+
+        if (errors) {
+            dispatch(reset());
+        }
     };
 
     const handleSubmit = (event) => {
         event.preventDefault();
         dispatch(getLogin(login, password));
+
+        if (errors) {
+            dispatch(reset());
+        }
     };
 
     const history = useHistory();
 
     if (token) {
         history.push(URLs.home.url);
-    }
-
-    if (errors) {
-        alert('Пользователь не найден');
     }
 
     const handleClick = (event) => {
@@ -54,6 +61,7 @@ const Login = ({ moveNextStep }) => {
     return (
         <React.Fragment>
             <form onSubmit={handleSubmit}>
+                {errors && <span>Пользователь не найден</span>}
                 <div>
                     <Input onChange={handleChangeLogin} label={t('auth.login.form.username.label')} id="username" name="username" type="text" placeholder="Elliot Page" />
                     <Input onChange={handleChangePassword} label={t('auth.login.form.password.label')} id="password" name="password" type="password" placeholder="••••••" />
