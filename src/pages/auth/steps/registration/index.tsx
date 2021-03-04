bf214832dc25e1c1b5ea3c3daeb87d60914027bb
melodoc <liubov_rz@yahoo.com>
2021-03-05 01:40:41 +0300

    FJ-84 Добавлены состояния на страницу регистрации

--- src/pages/auth/steps/registration/index.tsx
+++ src/pages/auth/steps/registration/index.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
-import { useTranslation } from 'react-i18next';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
 import { useHistory } from 'react-router-dom'
+import { useTranslation } from 'react-i18next';
 
 import { URLs } from '../../../../__data__/urls';
+import { getAccountData } from '../../../../__data__/action/registration';
+import { reset } from '../../../../__data__/slice/registration';
 
 import Button from '../../../../components/button'
 import Input from '../../../../components/input';
@@ -11,29 +15,82 @@ import style from './style.css';
 
 const Registration = ({ moveNextStep }) => {
     const { t } = useTranslation();
-    const handleClick = (event) => {
+
+    const dispatch = useDispatch();
+    const [login, setLogin] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const token = useSelector((state: any) => state.registration.token);
+    const errors = !!useSelector((state: any) => state.registration.errors);
+    console.log(errors);
+
+    const handleChangeLogin = (event) => {
+        setLogin(event.target.value);
+
+        if (errors) {
+            dispatch(reset());
+        }
+        
+    };
+
+    const handleChangeEmail = (event) => {
+        setEmail(event.target.value);
+
+        if (errors) {
+            dispatch(reset());
+        }
+    };
+
+    const handleChangePassword = (event) => {
+        setPassword(event.target.value);
+
+        if (errors) {
+            dispatch(reset());
+        }
+    };
+
+    const handleRepeatPassword = (event) => {
+        setPassword(event.target.value);
+
+        if (errors) {
+            dispatch(reset());
+        }
+    };
+
+
+    const handleSubmit = (event) => {
         event.preventDefault();
-        moveNextStep()
+        dispatch(getAccountData(login, email, password));
+
+
+        if (errors) {
+            dispatch(reset());
+        }
     }
 
     const history = useHistory();
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        alert('Зарегестрированы!');
+
+    if (token) {
         history.push(URLs.personalizations.url);
     }
+
+    const handleClick = (event) => {
+        event.preventDefault();
+        moveNextStep();
+    };
 
     return (
         <React.Fragment>
             <form onSubmit={handleSubmit}>
+                {errors && <span>{t('auth.registartion.form.error')}</span>}
                 <div>
-                    <Input label={t('auth.login.form.username.label')} id="username" name="username" type="text" placeholder="Elliot Page" />
-                    <Input label={t('auth.login.form.email.label')} id="userEmail" name="email" type="email" placeholder="email@example.com" />
-                    <Input label={t('auth.login.form.password.label')} id="password" name="password" type="password" placeholder="••••••" />
-                    <Input label={t('auth.login.form.repeat.password.label')} id="repeatPassword" name="repeatPassword" type="password" placeholder="••••••" />
+                    <Input onChange={handleChangeLogin} label={t('auth.login.form.username.label')} id="username" name="username" type="text" placeholder="Elliot Page" />
+                    <Input onChange={handleChangeEmail} label={t('auth.login.form.email.label')} id="userEmail" name="email" type="email" placeholder="email@example.com" />
+                    <Input onChange={handleChangePassword} label={t('auth.login.form.password.label')} id="password" name="password" type="password" placeholder="••••••" />
+                    <Input onChange={handleRepeatPassword} label={t('auth.login.form.repeat.password.label')} id="repeatPassword" name="repeatPassword" type="password" placeholder="••••••" />
                 </div>
                 <div className={style.buttons}>
-                    <Button onClick={handleSubmit}>
+                    <Button>
                         {t('auth.singup.button')}
                     </Button>
                 </div>
--- src/pages/personalization/steps/categories/index.tsx
+++ src/pages/personalization/steps/categories/index.tsx
@@ -1,15 +1,17 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import {useTranslation} from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 
-import {getThemes} from "../../../../__data__/action/themes";
-import Button from "../../../../components/button";
+import { getThemes } from '../../../../__data__/action/themes';
+
+import Button from '../../../../components/button';
 import List from '../../../../components/list';
+import { Loader } from '../../../../components/loader/loader';
 
 import style from './style.css';
 
 
-const ChooseCategories = ({moveNextStep}) =>{
+const ChooseCategories = ({ moveNextStep }) => {
   const { t } = useTranslation();
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -24,24 +26,24 @@ const ChooseCategories = ({moveNextStep}) =>{
   const { isLoading } = useSelector((state: any) => ({ isLoading: state.themes.loading }));
   const { themes } = useSelector((state: any) => ({ themes: state.themes.themes }));
   const { errors } = useSelector((state: any) => ({ errors: state.themes.errors }));
-  
+
   return (
     <React.Fragment>
-        <form onSubmit={handleSubmit}>
-          <div className={style.formWrapper}>
-            {isLoading ? <span> Loading... </span>
-              : themes && <List labels={themes} />}
-            {errors && <span>{errors.find((error) => error.field === 'themes').text}</span>}
-          </div>
-          <div className={style.buttons}>
-            <Button>
-              {t('personalizations.lang.button')}
-            </Button>
-          </div>
-        </form>
+      <form onSubmit={handleSubmit}>
+        <div className={style.formWrapper}>
+          {isLoading ? <Loader/>
+            : themes && <List labels={themes} />}
+          {errors && <span>{errors.find((error) => error.field === 'themes').text}</span>}
+        </div>
+        <div className={style.buttons}>
+          <Button>
+            {t('personalizations.lang.button')}
+          </Button>
+        </div>
+      </form>
     </React.Fragment>
   );
 }
-    
+
 
 export default ChooseCategories;
