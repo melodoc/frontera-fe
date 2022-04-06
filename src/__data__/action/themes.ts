c32040e0863b212c28c9f87ccac467dc91208fdb
Lyubov.Ryazanova <Lyubov.Ryazanova@waveaccess.ru>
2022-04-07 03:10:51 +0400

    change baseUrl to httpbin at personalizations module

--- src/__data__/action/themes.ts
+++ src/__data__/action/themes.ts
@@ -1,25 +1,24 @@
-import { getConfigValue } from '@ijl/cli';
+import { themesErr, themesRes } from 'api/themes/themes';
 import { success, error, init } from  '../slice/themes'
 
 export const getThemes = () => async (dispatch) => {
     dispatch(init());
-    const baseApiUrl = getConfigValue('frontera.api');
+    const baseApiUrl = 'https://httpbin.org/get';
 
-    const response = await fetch(`${baseApiUrl}/themes/success`, {
+    const response = await fetch(`${baseApiUrl}`, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json;charset=utf-8',
         }
     });
 
-
     if (response.ok) {
         const result = await response.json();
-        dispatch(success(result.themes));
+        dispatch(success(themesRes.themes));
     } else {
         try {
             const result = await response.json();
-            dispatch(error(result.errors));
+            dispatch(error(themesErr.errors));
         } catch (error) {
             console.error(error);
         }
