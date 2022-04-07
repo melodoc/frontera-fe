95766f0ff43110d92606c6e196f5fd3975d74d46
Lyubov.Ryazanova <Lyubov.Ryazanova@waveaccess.ru>
2022-04-07 04:23:22 +0400

    change baseUrl to httpbin at trending module

--- src/__data__/action/trending.ts
+++ src/__data__/action/trending.ts
@@ -1,11 +1,11 @@
-import { getConfigValue } from '@ijl/cli';
+import { trendingErr, trendingRes } from 'api/trending/trending';
 import { success, error, init } from  '../slice/trending'
 
 export const getCourses = () => async (dispatch) => {
     dispatch(init());
-    const baseApiUrl = getConfigValue('frontera.api');
+    const baseApiUrl = 'https://httpbin.org/get';
 
-    const response = await fetch(`${baseApiUrl}/trending/success`, {
+    const response = await fetch(`${baseApiUrl}`, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json;charset=utf-8',
@@ -14,12 +14,14 @@ export const getCourses = () => async (dispatch) => {
 
 
     if (response.ok) {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
         const result = await response.json();
-        dispatch(success(result.themes));
+        dispatch(success(trendingRes.themes));
     } else {
         try {
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
             const result = await response.json();
-            dispatch(error(result.errors));
+            dispatch(error(trendingErr.errors));
         } catch (error) {
             console.error(error);
         }
