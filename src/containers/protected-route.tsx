8a5626148c3c0d1f3f126f7049c57a09bbe37e04
nilienta <97403514+nilienta@users.noreply.github.com>
2023-02-09 14:37:06 +0400

    create component ProtectedRoute

---
+++ src/containers/protected-route.tsx
@@ -0,0 +1,17 @@
+import { FC } from 'react';
+import { Route, Redirect, useLocation, RouteProps } from 'react-router-dom';
+
+import { useAppSelector } from '__data__/store/hooks';
+
+export const ProtectedRoute: FC<RouteProps> = ({ children, ...rest }) => {
+  const { token } = useAppSelector((state) => state.login);
+  const location = useLocation();
+  if (!token) {
+    return (
+      <Route {...rest}>
+        <Redirect to={{ pathname: '/auth', state: { from: location } }} />
+      </Route>
+    );
+  }
+  return <Route {...rest}>{children}</Route>;
+};
