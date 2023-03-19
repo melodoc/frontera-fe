84f0137e367bfbf03f409382fb6544d77a2f6b82
melodoc <liubov_rz@yahoo.com>
2023-03-19 14:40:41 +0400

    Fix redirect from main issue. Issue: FE-52

--- src/hooks/use-auth-manager.ts
+++ src/hooks/use-auth-manager.ts
@@ -1,32 +1,37 @@
-import { useHistory } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
 
 import { dropToken, getToken } from 'services/token';
 import { URLs } from '__data__/urls';
 
+import { useRouterHelper } from './use-router-helper';
+
 export const useAuthManager = () => {
   const history = useHistory();
+  const location = useLocation();
+  const { isRootPath } = useRouterHelper();
 
   const logout = () => {
     history.push(URLs.auth.url);
     dropToken();
   };
 
-  const checkValidity = (
-    locationPathname = URLs.home.url,
-    onSuccessHandle?: () => void,
-    onErrorHandler?: () => void
-  ) => {
+  const getSafePathname = () => {
+    const currentPathname = location?.pathname ?? URLs.home.url;
+    return isRootPath(currentPathname) ? URLs.home.url : currentPathname;
+  };
+
+  const checkValidity = (onSuccessHandle?: () => void, onErrorHandler?: () => void) => {
     const token = getToken();
-    if (token) {
-      history.push(locationPathname);
+    if (!token) {
+      logout();
+      onErrorHandler && onErrorHandler();
+    } else {
+      history.push(getSafePathname());
       onSuccessHandle && onSuccessHandle();
       // TODO: Replace when authorization mechanism is added
       // dispatch(checkValidity(token));
       // TODO: Add request
       // setUserInformation();
-    } else {
-      logout();
-      onErrorHandler && onErrorHandler();
     }
   };
 
